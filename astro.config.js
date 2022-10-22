0c4bee964616640456561577efb4f1714f414f30
pan93412 <pan93412@gmail.com>
2022-10-22 22:49:03 +0800

    feat: → heading section

--- astro.config.js
+++ astro.config.js
@@ -4,8 +4,9 @@ import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    compress(),
-  ],
+  integrations: [compress(), image()]
 });
