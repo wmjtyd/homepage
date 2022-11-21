f11c373fed0411ae7e497384d3bd920b983d7b3b
“xweb” <xxxquotes@gmail.com>
2022-11-21 20:50:16 +0800

    fix: language

--- src/components/i18n/ChangeLanguage.tsx
+++ src/components/i18n/ChangeLanguage.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { HiLanguage  } from "react-icons/hi2";
 
 import { languages } from './resource';
 
@@ -8,7 +9,9 @@ export const ChangeLanguage = () => {
   console.log('router', router);
   return (
     <div className="dropdown-hover dropdown text-white">
-      <label className="btn m-1 text-white">{languages.find(val => val.type === router.locale)?.label || 'English'}</label>
+      <label className="btn m-1 text-white"><HiLanguage style={{
+        fontSize: '26px'
+      }} className="inline" />{languages.find(val => val.type === router.locale)?.label || 'English'}</label>
       <ul className="dropdown-content menu rounded-box w-52 bg-base-200 p-2 shadow">
         {languages.map((v) => (
           <li key={v.type}>
